9a93e64bf0a2dc14f0bb6f8186920228ed8f73bb
stephensanchez05 <stephensanchez05@gmail.com>
2020-05-16 15:56:52 -0400

    added scopes

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,8 @@
 class Character < ApplicationRecord
     belongs_to :user
-    has_many :heros
+    has_many :heros, :dependent => :delete_all
     has_many :spells, through: :heros
+
+    scope :low_level, -> { where("level < 10")}
+    scope :high_level, -> { where("level >= 10")}
 end
--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,5 +1,5 @@
 class Spell < ApplicationRecord
   belongs_to :user
-  has_many :heros
+  has_many :heros, :dependent => :delete_all
   has_many :characters, through: :heros
 end
